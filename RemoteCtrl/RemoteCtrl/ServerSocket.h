9a40462f2b876e6f61ce5202c47f6ce428c79dad
18803897962 <96913548+18803897962@users.noreply.github.com>
2022-09-29 20:32:19 +0800

    1、隐藏控制命令界面，后台运行 2、完成包的设计和实现

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -1,6 +1,71 @@
 #pragma once
 #include "pch.h"
 #include "framework.h"
+
+class CPacket   //声明数据包的类
+{
+public:
+	CPacket():sHead(0),nLength(0),sCmd(0),sSum(0) {}
+	CPacket(const CPacket& packet) {
+		sHead = packet.sHead;
+		nLength = packet.nLength;
+		sCmd = packet.sCmd;
+		strData = packet.strData;
+		sSum = packet.sSum;
+	}
+	CPacket(const BYTE* pData, size_t& nSize) {
+		size_t i = 0;   //数据段的偏移量
+		for (; i < nSize; i++) {
+			if (*(WORD*)(pData + i) == 0xFEFF) {
+				sHead = *(WORD*)(pData + i);
+				i += 2;
+				break;
+			}
+		}
+		if (i+4+2+2 > nSize) {  //包数据可能不全，或包头未能全部接收，解析失败
+			nSize = 0;
+			return;
+		}
+		nLength = *(DWORD*)(pData + i); i += 4;
+		if (nLength + i > nSize) {  //避免只解析一半的情况，包未完全接收到就返回，解析失败
+			nSize = 0;
+			return;
+		}
+		sCmd = *(WORD*)(nLength + i); i += 2;
+		if (nLength > 4) {
+			strData.resize(nLength - 2 - 2);
+			memcpy((void*)strData.c_str(), pData + i, nLength - 4);
+			i += nLength - 4;
+		}
+		sSum = *(WORD*)(pData + i); i += 2;
+		WORD sum=0;  //进行校验
+		for (size_t j = 0; j < strData.size(); j++) {
+			sum += BYTE(strData[i]) & 0xFF;
+		}
+		if (sum == sSum) { //解析成功
+			nSize = i ;  //此时的i是包的实际长度
+			return;
+		}
+		nSize = 0;  //解析失败
+	}
+	~CPacket() {}
+	CPacket& operator=(const CPacket& pack) {
+		if (this != &pack) {
+			sHead = pack.sHead;
+			nLength = pack.nLength;
+			sCmd = pack.sCmd;
+			strData = pack.strData;
+			sSum = pack.sSum;
+		}
+		return *this;
+	}
+public:
+	WORD sHead;  //包头，固定位FEFF
+	DWORD nLength;  //包长度，控制命令开始到和校验结束
+	WORD sCmd; //控制命令
+	std::string strData;  //数据
+	WORD sSum;  //和校验
+};
 class CServerSocket
 {
 public:
@@ -16,10 +81,7 @@ public:
 		serv_adr.sin_family = AF_INET;
 		serv_adr.sin_addr.s_addr = INADDR_ANY;
 		serv_adr.sin_port = htons(9527);
-		if (bind(m_sock, (sockaddr*)&serv_adr, sizeof(serv_adr)) == -1) {
-			printf("%d\n", GetLastError());
-			return false;
-		}
+		if (bind(m_sock, (sockaddr*)&serv_adr, sizeof(serv_adr)) == -1) 	return false;
 		if (listen(m_sock, 1) == -1) return false;
 		return true;
 	}
@@ -30,17 +92,28 @@ public:
 		m_client =accept(m_sock, (sockaddr*)&client_adr, &cli_sz);
 		if (m_client == -1) return false;
 		return true;
-//             recv(serv_sock, buffer, sizeof(buffer), 0);
-//             send(serv_sock, buffer, sizeof(buffer), 0);
 	}
+#define BUFFER_SIZE 4096
 	int DealCommand() {
 		if (m_client == -1) return -1;
-		char buffer[1024];
+		//char buffer[1024]="";
+		char* buffer = new char[BUFFER_SIZE];
+		memset(buffer, 0, BUFFER_SIZE);
+		size_t index = 0;
 		while (true) {
-			int len = recv(m_client, buffer, sizeof(buffer), 0);
+			size_t len = recv(m_client, buffer, BUFFER_SIZE-index, 0);
 			if (len <= 0) return -1;
 			//TODO:处理命令
+			index +=len;  //这个len是收到数据包的长度
+			len = index;
+			m_packet=CPacket((BYTE*)buffer, index);
+			if (len > 0) {
+				memmove(buffer, buffer + len, BUFFER_SIZE-len);  //这个len是实际上有效的数据包长度
+				index -= len;
+				return m_packet.sCmd;
+			}
 		}
+		return -1;
 	}
 	bool Send(const char* pData, size_t nSize) {
 		return send(m_client, pData, nSize, 0)>0?true:false;
@@ -48,6 +121,7 @@ public:
 private:
 	SOCKET m_sock;
 	SOCKET m_client;
+	CPacket m_packet;
 	CServerSocket& operator=(const CServerSocket& ss) {}
 	CServerSocket(const CServerSocket& ss) {
 		m_client = ss.m_client;
