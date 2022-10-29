8d0708c87fa36df3542d9c47d85be653d833507d
18803897962 <96913548+18803897962@users.noreply.github.com>
2022-10-29 23:11:11 +0800

    1、追加启动权限请求 2、设置开机启动 提示：shell命令时加空格会导致命令格式错误

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -13,6 +13,46 @@
 // 唯一的应用程序对象
 CWinApp theApp;
 using namespace std;
+
+void ChooseAutoInvoke() {//自动启动
+    CString strSubKey=_T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+    CString strInfo = _T("该程序只允许用于合法用途！\n");
+    strInfo += _T("继续运行该程序将使得该机器处于被监控状态！\n");
+	strInfo += _T("如果你不希望，请按取消按钮退出程序。\n");
+	strInfo += _T("按下“是”按钮，该程序将被复制到你的机器，随系统自动运行。\n");
+	strInfo += _T("按下“否”按钮，该程序值运行依次，将不会在系统中残留。\n");
+    int ret=MessageBox(NULL,strInfo,_T("警告"),MB_YESNOCANCEL|MB_ICONWARNING|MB_TOPMOST);
+    if (ret == IDYES) {
+        char sPath[MAX_PATH] = "";
+        char sSys[MAX_PATH] = "";
+        std::string strExe = "\\RemoteCtrl.exe ";
+        GetCurrentDirectoryA(MAX_PATH, sPath);
+        GetSystemDirectoryA(sSys, sizeof(sSys));
+        std::string strCmd = "cmd /K mklink "+std::string(sSys)+strExe + std::string(sPath) + strExe;  //创建链接
+        ret=system(strCmd.c_str());
+        TRACE("ret=%d\r\n", ret);
+        //注册表操作
+        HKEY hKey=NULL;
+        ret=RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS|KEY_WOW64_64KEY, &hKey);  
+        if (ret != ERROR_SUCCESS) {
+            RegCloseKey(hKey);
+            MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败"),_T("错误"),MB_ICONERROR|MB_TOPMOST);
+            exit(0);
+        }
+        CString strPath = (CString)(_T("%SystemRoot%\\SysWOW64\\RemoteCtrl.exe"));
+        ret=RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ,(BYTE*)(LPCTSTR)strPath,strPath.GetLength()*sizeof(TCHAR));
+        if (ret != ERROR_SUCCESS) {
+			RegCloseKey(hKey);
+			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+			exit(0);
+        }
+        RegCloseKey(hKey);
+    }
+    else if (ret == IDCANCEL) {
+        exit(0);
+    }
+    return;
+}
 int main()
 {
     int nRetCode = 0;
@@ -32,6 +72,7 @@ int main()
         {
             // TODO: 在此处为应用程序的行为编写代码。
             CCommand cmd;
+            ChooseAutoInvoke();
             int ret= CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);
             switch (ret)
             {
