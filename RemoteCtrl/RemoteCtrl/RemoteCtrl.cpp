0b59d53580ad5ba0278a50b09cf680dd09a14a5b
18803897962 <96913548+18803897962@users.noreply.github.com>
2023-03-07 12:21:26 +0800

    完成iocp的一个example，并解决线程退出导致的内存泄漏问题

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -7,6 +7,7 @@
 #include "ServerSocket.h"
 #include "Tools.h"
 #include "Command.h"
+#include <conio.h>
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -49,8 +50,83 @@ bool ChooseAutoInvoke(const CString& strPath) {//自动启动
     return true;
 }
 
+#define IOCP_LIST_EMPTY 0
+#define IOCP_LIST_PUSH 1
+#define IOCP_LIST_POP 2
+enum {
+	iocp_list_empty,
+	iocp_list_push,
+	iocp_list_pop
+};
+
+//iocp完成端口映射
+int messcount = 0;
+int delecount = 0;
+struct IOCP_Param {
+	int	nOperator;//插入or删除
+	std::string strData;
+	_beginthread_proc_type cbFunc;//回调函数
+	IOCP_Param(int op, const char* sData, _beginthread_proc_type cbfunc = NULL) {
+		nOperator = op;
+		strData = sData;
+		cbFunc = cbfunc;
+	}
+	IOCP_Param() :nOperator(-1) {}
+}IOCPPARAM;
+
+void threadPlay(HANDLE hIOCP) {
+	std::list<std::string> lstString;
+	DWORD dwTransfered = 0;
+	ULONG_PTR CompletionKey = 0;
+	OVERLAPPED* pOverlaped = 0;
+	while (GetQueuedCompletionStatus(hIOCP, &dwTransfered, &CompletionKey, &pOverlaped, INFINITE)) {
+		if (dwTransfered == 0 || CompletionKey == NULL) {  //主动按下键盘，结束信号的发送时，就代表iocp已经退出
+			printf("thread is prapare to exit!\n");
+			break;
+		}
+		IOCP_Param* pParam = (IOCP_Param*)CompletionKey;
+		if (pParam->nOperator == iocp_list_push) {
+			lstString.push_back(pParam->strData);
+			printf("push\n");
+		}
+		else if (pParam->nOperator == iocp_list_pop) {
+			std::string* pStr = NULL;
+			if (lstString.size() > 0) {
+				pStr = new std::string(lstString.front());
+				lstString.pop_front();
+			}
+			if (pParam->cbFunc) {
+				pParam->cbFunc(pStr);
+			}
+		}
+		else if (pParam->nOperator == iocp_list_empty) {
+			lstString.clear();
+		}
+		delete pParam;
+		delecount++;
+	}
+}
+
+void threadQueEntry(HANDLE hIOCP) {
+	threadPlay(hIOCP);  //这种做法，确保线程函数中的变量在函数结束时被释放，避免_endthread()调用时，变量未被释放而导致内存泄漏
+	_endthread();
+}
+
+void func(void* arg) {
+	std::string* pstr = (std::string*)arg;
+	if (pstr != NULL) {
+		printf("pop from list:%s\n", pstr->c_str());
+	}
+	else {
+		printf("the list is empty\n");
+	}
+	delete pstr;
+}
+
+
+
 int main()
-{
+{/*
 	if (CTools::IsAdmin()) {  //管理员
 		if (!CTools::init()) return 1;
 		//OutputDebugString(L"current is run as administrator!\r\n");
@@ -74,6 +150,40 @@ int main()
             CTools::ShowError();
 			return 1;
         }
+	}*/
+
+	if (!CTools::init()) return 1;//完成端口映射
+	HANDLE hIOCP = INVALID_HANDLE_VALUE;//IOCP IO Completion PORT
+	hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 1);//1表示能够同时访问的线程数 设置为1，只有一个线程来处理队列 可以由多个线程需要该线程处理消息队列
+	if (hIOCP == INVALID_HANDLE_VALUE||hIOCP==NULL) {
+		printf("create iocp failed %d\n", GetLastError());
 	}
+	HANDLE hThread=(HANDLE) _beginthread(threadQueEntry, 0, hIOCP);
+	DWORD tick = GetTickCount64();
+	DWORD tick0 = GetTickCount64();
+	while (_kbhit()==0) {
+		if (GetTickCount64() - tick0 > 1300) {
+			PostQueuedCompletionStatus(hIOCP,sizeof(IOCP_Param), (ULONG_PTR)new IOCP_Param(iocp_list_pop, "hello world",func), NULL);
+			messcount++;
+			tick0 = GetTickCount64();
+		}
+		if (GetTickCount64() - tick > 2000) {
+			PostQueuedCompletionStatus(hIOCP,sizeof(IOCP_Param), (ULONG_PTR)new IOCP_Param(iocp_list_push,"hello world"), NULL);
+			messcount++;
+			tick = GetTickCount64();
+		}
+		Sleep(1);
+	}
+	if (hIOCP != INVALID_HANDLE_VALUE) {
+		//TODO:唤醒完成端口 线程需要结束
+		PostQueuedCompletionStatus(hIOCP, sizeof(IOCP_Param), (ULONG_PTR)new IOCP_Param(iocp_list_empty, "hello world"), NULL);//传递一个状态
+		messcount++;
+		PostQueuedCompletionStatus(hIOCP, 0, NULL, NULL);
+		WaitForSingleObject(hThread,INFINITE);//等待线程结束
+	}
+	printf("exit\n");
+	printf("messcount=%d  delecount=%d\n", messcount, delecount);
+	CloseHandle(hIOCP);
+	::exit(0);
     return 0;
 }
